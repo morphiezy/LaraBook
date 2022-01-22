b682ee4c6dcc0ac6950b9dcceda335a27d2511da
morphiezy <myardan.aqiella22@gmail.com>
2022-01-22 12:58:41 +0700

    Delete Comments

--- app/Http/Controllers/BukuController.php
+++ app/Http/Controllers/BukuController.php
@@ -7,11 +7,7 @@ use Illuminate\Http\Request;
 
 class BukuController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+   
     public function index()
     {
         $bukus = Buku::latest()->paginate(10);
@@ -19,22 +15,11 @@ class BukuController extends Controller
         -> with('i', (request()->input('page', 1) - 1) * 2);
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *  
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
     public function store(Request $request)
     {
         $request->validate([
@@ -43,40 +28,15 @@ class BukuController extends Controller
         ]);
   
         Buku::create($request->all());
-   
         return redirect()->route('index')->with('success','Buku Berhasil Dibuat.');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Buku  $Buku
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Buku $Buku)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Buku  $Buku
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     { 
         $data = Buku::find($id);
         return view('edit',compact('data'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Buku  $Buku
-     * @return \Illuminate\Http\Response
-     */
     public function update(Request $request , $id)
     {
         $this->validate($request,[
@@ -89,21 +49,13 @@ class BukuController extends Controller
         $post->pengarang = $request->input('pengarang');
         $post->save();
   
-        return redirect()->route('index')
-                        ->with('success','Buku Berhasil Diubah');
+        return redirect()->route('index')->with('success','Buku Berhasil Diubah');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Buku  $Buku
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
         Buku::where('id',$id)->delete();
-        return redirect()->route('index')
-                        ->with('success','Buku Berhasil dihapus');
+        return redirect()->route('index')->with('success','Buku Berhasil dihapus');
 
     }
 }
